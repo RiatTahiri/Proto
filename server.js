147e7c6577787c883ac06a39b48d3d539ecc7563
Rijad T <badvidgamer@gmail.com>
2024-10-19 00:19:02 +0200

    ejs integration

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-require('dotenv').config()
+let ejs = require('ejs');
+let path = require('path');
 
 const express = require("express");
 const app = express();
 
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/src/views/'));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render('home')
 });
 
-const port = 3000
 
+
+
+
+
+
+const port = 3000
 app.listen(port, ()=> console.log(`Dolphin app listening on port ${port}!`))
