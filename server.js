bd59635b0fab7e170a8b75bcbe9bd473dbc8f6e5
Rijad T <badvidgamer@gmail.com>
2024-10-18 23:33:46 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = 3000
+
+app.listen(port, ()=> console.log(`Dolphin app listening on port ${port}!`))
